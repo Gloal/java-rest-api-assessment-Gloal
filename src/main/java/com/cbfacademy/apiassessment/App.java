c96c5c4d274f176917543a30e468e02a9ae444b6
Gloal <53401314+Gloal@users.noreply.github.com>
2023-11-06 01:16:52 +0000

    Revert "Test endpoints"

--- src/main/java/com/cbfacademy/apiassessment/App.java
+++ src/main/java/com/cbfacademy/apiassessment/App.java
@@ -1,21 +1,13 @@
 package com.cbfacademy.apiassessment;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-
 @SpringBootApplication
 @RestController
-// @EntityScan("com.cbfacademy.apiassessment")
-// @EnableJpaRepositories(basePackages = {"com.cbfacademy.apiassessment"})
 public class App {
 
 	public static void main(String[] args) {
